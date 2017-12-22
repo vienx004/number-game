4be6cde2b458c60965416f7a7091ea9c17bcdb20
vienx004 <derek.h.vien@gmail.com>
2017-12-21 23:58:06 -0600

    create number-game

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+sumOfNum: number = 0;
+topNum: number = Math.floor(Math.random() * 100) + 1;
+num1: number = this.randombtwn10();
+num2: number = this.randombtwn10();
+num3: number = this.randombtwn10();
+num4: number = 1;
 
-  constructor(public navCtrl: NavController) {
+//this is a function on event listener click to add to my number
+  onClick(num: number){
+    this.sumOfNum = this.sumOfNum + +num;
+    console.log(this.sumOfNum);
+    this.randomNum();
+  }
 
+  onSubmit(){
+    if(this.sumOfNum == this.topNum)
+    {
+      console.log("Winner");
+      this.resetSum();
+    }else if(this.sumOfNum !== this.topNum){
+      console.log("Loser");
+      alert
+      this.resetSum();
+    }
+  }
+
+  resetSum(){
+    this.sumOfNum = 0;
+    this.topNum = Math.floor(Math.random() * 100) + 1;
+    this.resultOfGame = "Chong";
+  }
+
+  randomNum(num1: number, num2: number, num3: number){
+    this.num1 = this.randombtwn10();
+    this.num2 = this.randombtwn10();
+    this.num3 = this.randombtwn10();
+  }
+
+  randombtwn10(num: number){
+    return num = Math.floor(Math.random() * 10) + 1;
   }
 
 }
