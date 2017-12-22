f3a8317e28c819af20e625bd7cf442f33cb7bcd6
vienx004 <derek.h.vien@gmail.com>
2017-12-22 15:39:37 -0600

    added disable button features, prettified UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,6 +15,9 @@ start: number
 diff: number
 resultOfGame: string
 resetFlag: boolean
+showTimerFlag: boolean
+showResultFlag: boolean
+disableBtnFlag: boolean
 
 ionViewDidLoad(){
   this.sumOfNum = 0;
@@ -27,6 +30,9 @@ ionViewDidLoad(){
   this.diff = 0;
   this.resultOfGame = "";
   this.resetFlag = false;
+  this.showTimerFlag = false;
+  this.showResultFlag = false;
+  this.disableBtnFlag = false;
 }
 
 //this is a function on event listener click to add to my number
@@ -41,25 +47,31 @@ ionViewDidLoad(){
     {
       this.diff = Math.floor(this.diffTime()/1000);
       this.resultOfGame = "Winner";
+      this.showTimerFlag = true;
     }else if(this.sumOfNum !== this.topNum){
       this.resultOfGame = "Loser";
     }
     this.resetFlag = true;
+    this.showResultFlag = true;
+    this.disableBtnFlag = true;
   }
 
   onReset(){
     this.start = this.startTime();
     this.resetSum();
     this.resetFlag = false;
+    this.showTimerFlag = false;
+    this.showResultFlag = false;
+    this.disableBtnFlag = false;
+    this.randomNum(this.num1, this.num2, this.num3);
   }
 
   resetSum(){
     this.sumOfNum = 0;
     this.topNum = Math.floor(Math.random() * 100) + 1;
-
   }
 
-  randomNum(num1: number, num2: number, num3: number){
+  randomNum(num: number, num2: number, num3: number){
     this.num1 = this.randombtwn10();
     this.num2 = this.randombtwn10();
     this.num3 = this.randombtwn10();
@@ -79,6 +91,13 @@ ionViewDidLoad(){
   
   isReset(){
     if(this.resetFlag == false){
+      return false;
+    }
+    return true;
+  }
+
+  isShowTimer(){
+    if(this.showTimerFlag == false){
       return false;
     }
     return true;
